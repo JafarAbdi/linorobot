dc2d37809978752e553ba80f2c98f8ccd36a6316
JafarAbdi <cafer.abdi@gmail.com>
2019-04-03 12:44:12 +0000

    Update the robot's specification

--- teensy/firmware/lib/config/lino_base_config.h
+++ teensy/firmware/lib/config/lino_base_config.h
@@ -25,12 +25,12 @@
 #define K_I 0.3 // I constant
 #define K_D 0.5 // D constant
 
-//define your robot' specs here
-#define MAX_RPM 330               // motor's maximum RPM
-#define COUNTS_PER_REV 1550       // wheel encoder's no of ticks per rev
-#define WHEEL_DIAMETER 0.10       // wheel's diameter in meters
+//define your robot' specs here ### SEE ../Links/Motor for more info.
+#define MAX_RPM 210               // motor's maximum RPM
+#define COUNTS_PER_REV 341.2       // wheel encoder's no of ticks per rev
+#define WHEEL_DIAMETER 0.065       // wheel's diameter in meters
 #define PWM_BITS 8                // PWM Resolution of the microcontroller
-#define LR_WHEELS_DISTANCE 0.235  // distance between left and right wheels
+#define LR_WHEELS_DISTANCE 0.240  // distance between left and right wheels
 #define FR_WHEELS_DISTANCE 0.30   // distance between front and rear wheels. Ignore this if you're on 2WD/ACKERMANN
 #define MAX_STEERING_ANGLE 0.415  // max steering angle. This only applies to Ackermann steering
 
